689d9ef260e68863275f020a52c92f628233eead
abelpeterson.dream <abelpeterson.dream@gmail.com>
2022-12-08 08:36:24 -0500

    milestone1-finish

--- contracts/DivineRoi.sol
+++ contracts/DivineRoi.sol
@@ -11,56 +11,85 @@ import "hardhat/console.sol";
 import "./AggregatorV3Interface.sol";
 
 contract DivineRoi is Ownable {
-    address public admin;
-    uint256 public minDeposit;
-    uint256 public maxDeposit;
-    IERC721 public nft;
-    mapping(address => Deposit) deposits;
-    mapping(address => address) referers;
-    mapping(address => uint256) refers;
-    AggregatorV3Interface priceFeed;
-
+    
     struct Deposit {
         uint256 amount;
         uint256 deposit_time;
     }
 
+    struct DepositSet{
+        uint128 start;
+        uint128 end;
+        mapping(uint128 => Deposit) deposits;
+    }
+
+    address public admin;
+    mapping(address => DepositSet) private _depositInfo;
+    uint256 private _minDeposit;
+    uint256 private _maxDeposit;
+    IERC721 private _nft;
+    mapping(address => address) private _referers;
+    mapping(address => uint256) private _refers;
+    
+    AggregatorV3Interface private _priceFeed;
+
+    
     event DepositSuccess(address sender, uint256 amount);
     event SetReferer(address referer, address referee);
 
-    constructor(){
-        admin = _msgSender();
-        priceFeed = AggregatorV3Interface(0xAB594600376Ec9fD91F8e885dADF0CE036862dE0);
-        minDeposit = 10*(1e18);
-        maxDeposit = 5000*(1e18);
+    constructor(address aggregatorAddr){
+        _priceFeed = AggregatorV3Interface(aggregatorAddr);
+        _minDeposit = 10*(1e18);
+        _maxDeposit = 5000*(1e18);
     }
 
-    modifier onlyAdmin {
-        require(_msgSender() == admin, "only admin allowed");
-        _;
+    function setMinDeposit(uint256 minDeposit_) external onlyOwner {
+        _minDeposit = minDeposit_ * (1e18);
     }
 
-    function setMinDeposit(uint256 _amount) public onlyAdmin {
-        minDeposit = _amount * (1e18);
+    function setMaxDeposit(uint256 maxDeposit_) public onlyOwner {
+        _maxDeposit = maxDeposit_ * (1e18);
     }
 
-    function setMaxDeposit(uint256 _amount) public onlyAdmin {
-        maxDeposit = _amount * (1e18);
+    function getMinDepositInMatic() public view returns (uint256){
+        uint256 price = getLatestPrice();
+        return (_minDeposit/price)*1e18;
+    }
+
+    function getMaxDepositInMatic() public view returns (uint256){
+        uint256 price = getLatestPrice();
+        return (_maxDeposit/price)*1e18;
     }
 
     function deposit() external payable {
-        uint256 price = getLatestPrice();
-        require(msg.value > (minDeposit/price)*1e18, "insufficient amount!");
-        require(msg.value < (maxDeposit/price)*1e18, "exceed the maximum amount!");
-        deposits[_msgSender()].amount = msg.value;
-        deposits[_msgSender()].deposit_time = block.timestamp;
-        console.log("deposit_amount:", msg.value);
-        console.log("msg_sender:", _msgSender());
+        uint256 minLimit = getMinDepositInMatic();
+        uint256 maxLimit = getMaxDepositInMatic();
+        require(msg.value > minLimit, "insufficient amount!");
+        require(msg.value < maxLimit, "exceed the maximum amount!");
+        _updateDepositInfo(_msgSender()); // remove expired deposit before new Deposit.
+        uint128 newIdx = _depositInfo[_msgSender()].end;
+        _depositInfo[_msgSender()].deposits[newIdx].amount = msg.value;
+        _depositInfo[_msgSender()].deposits[newIdx].deposit_time = block.timestamp;
+        _depositInfo[_msgSender()].end ++;
         emit DepositSuccess(_msgSender(), msg.value);
     }
 
-    function getLatestPrice() internal pure returns (uint256){
-        return 10**18;
+    function _updateDepositInfo(address addr) internal {
+        uint128 idx = _depositInfo[addr].start;
+        uint256 before120DaysTime = block.timestamp - (120 days);
+        while(_depositInfo[addr].deposits[idx].deposit_time != 0 && 
+            _depositInfo[addr].deposits[idx].deposit_time < before120DaysTime) 
+        {
+            _depositInfo[addr].start ++;
+        }
+    }
+
+    function getDepositInfo(address addr, uint128 idx) public view returns (Deposit memory){
+        return _depositInfo[addr].deposits[idx];
+    }
+    
+    function getLatestPrice() public pure returns (uint256){
+        return 1e18;
         //  (
         //     /*uint80 roundID*/,
         //     int price,
@@ -71,22 +100,22 @@ contract DivineRoi is Ownable {
         // return price;
     }
 
-    function setReferer(address _referer) external {
-        require(referers[_msgSender()] == address(0), "Referer already set!");
-        require(_msgSender() != _referer, "Referer already set!");
-        require(deposits[_msgSender()].amount != 0, "You need to deposit before setReferer!");
-        referers[_msgSender()] = _referer;
-        refers[_referer] = refers[_referer] + 1;
-        emit SetReferer(_referer, _msgSender());
-    }
+    // function setReferer(address _referer) external {
+    //     require(_referers[_msgSender()] == address(0), "Referer already set!");
+    //     require(_msgSender() != _referer, "Referer already set!");
+    //     //require(_deposits[_msgSender()].amount != 0, "You need to deposit before setReferer!");
+    //     _referers[_msgSender()] = _referer;
+    //     _refers[_referer] = _refers[_referer] + 1;
+    //     emit SetReferer(_referer, _msgSender());
+    // }
 
-    function _calculateBasicBonus() internal view returns(uint256){
-        uint256 currentTime = block.timestamp;
-        uint256 passedDays = (currentTime - deposits[_msgSender()].deposit_time)/(1 days);
-        if(passedDays > 120) passedDays = 120;
-        uint256 basicBonus = deposits[_msgSender()].amount * passedDays / 100;
-        return basicBonus;
-    }
+    // function _calculateBasicBonus() internal view returns(uint256){
+    //     uint256 currentTime = block.timestamp;
+    //     uint256 passedDays = (currentTime - deposits[_msgSender()].deposit_time)/(1 days);
+    //     if(passedDays > 120) passedDays = 120;
+    //     uint256 basicBonus = deposits[_msgSender()].amount * passedDays / 100;
+    //     return basicBonus;
+    // }
 
     // function _calculateHolderBonus() internal view returns(uint256){
 
--- test/Lock.js
+++ test/Lock.js
@@ -10,11 +10,12 @@ describe("Lock", function () {
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
   it("Deployment", async function () {
-    //const [owner] = await ethers.getSigner();
+   // const [owner] = await ethers.getSigner();
     const roi = await ethers.getContractFactory("DivineRoi");
-    const roiContract = await roi.deploy();
-    const price = await roiContract.deposit({value:ethers.utils.parseEther("15")});
-    console.log("asdf:", price)
+    const roiContract = await roi.deploy("0xAB594600376Ec9fD91F8e885dADF0CE036862dE0");
+    await roiContract.deposit({value:ethers.utils.parseEther("15")});
+    const info = await roiContract.getDepositInfo("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266", 0)
+    console.log("asdf:", info)
   });
 
 
